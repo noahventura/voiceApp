e1435537499a7351cb1ca8e166e3f780de9179d5
ventunoa <60230236+ventunoa@users.noreply.github.com>
2023-08-11 23:05:44 -0400

    Moved getTime to function for readability

--- script.js
+++ script.js
@@ -22,14 +22,23 @@ recognition.onresult = function(event) {
   // Custom Commands
   if (transcript.toLowerCase().startsWith('open ')) {
     window.open('http://' + transcript.slice(5));
-  } else if (transcript.toLowerCase().startsWith('change background to ')) {
+  } else if (transcript.toLowerCase().startsWith('change background to ') || transcript.toLowerCase().startsWith('change background to ')) {
     document.body.style.backgroundColor = transcript.slice(21);
   } else if (transcript.toLowerCase() === 'tell me a joke') {
     document.getElementById('output').innerHTML = 'Why don’t scientists trust atoms? Because they make up everything!';
   } else if (transcript.toLowerCase().startsWith('search for ')) {
     window.open('https://www.google.com/search?q=' + encodeURIComponent(transcript.slice(11)));
   } else if (transcript.toLowerCase().startsWith('what time is it')) {
-  fetch('https://api.ipify.org?format=json')
+  getTime().then(time => {
+      document.getElementById('output').innerHTML = time;
+    });
+}
+
+  // Add more custom commands as needed
+};
+
+function getTime() {
+  return fetch('https://api.ipify.org?format=json')
     .then(response => response.json())
     .then(data => {
       const ipAddress = data.ip;
@@ -40,22 +49,17 @@ recognition.onresult = function(event) {
     .then(data => {
       const timeZone = data.timezone;
 
-      // Step 3: Calculate the local time based on the time zone
+      // Calculate the local time based on the time zone
       const localTime = moment.tz(timeZone);
-      console.log('Local time:', localTime.format('YYYY-MM-DD HH:mm:ss'));
-      document.getElementById('output').innerHTML = 'Local time: ' + localTime.format('YYYY-MM-DD HH:mm:ss');
+      return 'Local time: ' + localTime.format('YYYY-MM-DD HH:mm:ss');
     })
     .catch(error => {
       console.error('An error occurred:', error);
     });
 }
 
-  // Add more custom commands as needed
-};
-
-
   // Event triggered when an error occurs
   recognition.onerror = function(event) {
-    document.getElementById('output').innerHTML = 'An error occurred: ' + event.error;
+    return ('An error occurred: ' + event.error)
   };
 }
